ecaa91d1fd4b088739d9dd7e685517f68c293746
Alexander Phillips <bigtear@vip.qq.com>
2023-03-26 13:36:08 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,5 +3,14 @@ define(function (require, exports, module) {
 
     var ExtensionUtils = brackets.getModule("utils/ExtensionUtils");
 
-    ExtensionUtils.addEmbeddedStyleSheet(".CodeMirror{text-rendering: optimizeLegibility;font-family:'JetBrains Mono','LXGW WenKai GB Screen R','Segoe UI Emoji' !important;}");
+    ExtensionUtils.addEmbeddedStyleSheet(`
+html, body, input, button, select, textarea, .CodeMirror {
+  font-family:'JetBrains Mono','LXGW WenKai GB Screen R','Segoe UI Emoji' !important;
+}
+:root {
+  --sansFontFamily: 'JetBrains Mono','LXGW WenKai GB Screen R','Segoe UI Emoji' !important;
+  --font-family-sans-serif: 'JetBrains Mono','LXGW WenKai GB Screen R','Segoe UI Emoji' !important;
+  --font-family-monospace: 'JetBrains Mono','LXGW WenKai GB Screen R','Segoe UI Emoji' !important;
+}
+`);
 });
